730d3810b88f53f4d0739473ef49ef2c29783313
Harshit Mani Tripathi <hmanitops@gmail.com>
2022-08-21 19:01:38 +0530

    htop mhz removed

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -545,7 +545,8 @@ awful.rules.rules = {
             "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
             "Wpa_gui",
             "veromix",
-            "xtightvncviewer"
+            "xtightvncviewer",
+            -- "Brave"
         },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
