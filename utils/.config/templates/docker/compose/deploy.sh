a5ab186f747cfc457164378423088d26ed5e4e7d
Harshit Mani Tripathi <hmanitops@gmail.com>
2025-03-07 00:28:10 +0530

    moved shebang to top in deploy.sh

--- utils/.config/templates/docker/compose/deploy.sh
+++ utils/.config/templates/docker/compose/deploy.sh
@@ -1,12 +1,14 @@
-### Credit: https://askubuntu.com/a/957278
-### MAKE SURE TO USE BASH not DASH
-## Define array of all dirs to be created like:
-# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
-
-
 #!/bin/bash
+set -e
 CREATE_DIRS=( "${BAK_CFG_DIR}"/serviceName/{multiple,dir,names} "$DB_CFG_DIR"/serviceNameDb "$DB_CFG_DIR"/authentik_postgres )
 mkdir -p "${CREATE_DIRS[@]}"
 # ls -alh "$BAK_CFG_DIR"/serviceName/ "$DB_CFG_DIR"
 ls -alh "${CREATE_DIRS[@]}"
 docker compose up -d
+
+
+### Credit: https://askubuntu.com/a/957278
+### MAKE SURE TO USE BASH not DASH
+## Define array of all dirs to be created like:
+# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
+
